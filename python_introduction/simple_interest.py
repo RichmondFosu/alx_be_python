6d4baf6e0f1484967c352a227cffb799e02e10ef
Richmond Fosu <fosuserebour@gmail.com>
2025-09-11 21:21:04 +0100

    initial commit

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+
+# define the variables
+principal = 1000   # initial investment
+rate = 0.05        # 5% annual interest rate (as decimal)
+time = 3           # investment duration in years
+
+# Step 2: calculate simple interest
+interest = principal * rate * time
+
+# Step 3: print the result
+print(f"The simple interest is: {interest}")
