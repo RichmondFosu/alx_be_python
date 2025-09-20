ef7536a5dd0a39e2983c8fd42cb9726dadc690db
Richmond Fosu <fosuserebour@gmail.com>
2025-09-20 19:52:47 +0100

    Add multiplication table generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@
 number = int(input('Enter a number to see its multiplication table: '))
 
 #Generate and print multiplication table
-for num in range(1,10):
+for num in range(1,11):
      product = number * num
      print(f'{number} x {num} = {product}')
