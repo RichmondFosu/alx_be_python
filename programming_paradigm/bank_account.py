ebeaa7c566789e5322e65c7c13e6bbd08c414e98
Richmond Fosu <fosuserebour@gmail.com>
2025-10-05 16:34:23 +0100

    Initial commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        """Initialize a bank account with an optional starting balance (default = 0)."""
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        """Deposit money into the account if the amount is positive."""
+        if amount > 0:
+            self.account_balance += amount
+            print(f'Deposited: ${amount}')
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        """Withdraw money if sufficient funds exist. Return True if successful, else False."""
+        if amount > self.account_balance:
+            print("Insufficient funds.")
+            return False
+        else:
+            self.account_balance -= amount
+            print(f'Withdrew: ${amount}')
+            return True
+
+    def display_balance(self):
+        """Print the current balance in a user-friendly format."""
+        print(f'Current Balance: ${self.account_balance}')
