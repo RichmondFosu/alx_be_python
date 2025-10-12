cbbf01bfc125f10eeb6bf66c5b96d19a5843363a
Richmond Fosu <fosuserebour@gmail.com>
2025-10-12 16:11:02 +0100

    modify __repr__ method

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,7 +4,6 @@ class Book:
         self.author = author
         self.year = year
 
-
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
@@ -12,4 +11,4 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
     def __del__(self):
-        print(f"Deleting {self.title}.")
+        print(f"Deleting {self.title}")
